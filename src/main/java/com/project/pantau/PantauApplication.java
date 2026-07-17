daff67096d7482e9db681df94d49e9be5c0c8485
equinox-jj <joshua.jeremia@hotmail.com>
2026-07-17 14:36:20 +0700

    initial commit

---
+++ src/main/java/com/project/pantau/PantauApplication.java
@@ -0,0 +1,13 @@
+package com.project.pantau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PantauApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/pantau/PantauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pantau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

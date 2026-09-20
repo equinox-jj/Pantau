aa7ab8cda2db55bec1c35e6e7f3a674c946bef05
equinox-jj <joshua.jeremia@hotmail.com>
2026-09-20 21:05:40 +0700

    implement private method for uploads and delete photos

--- internal/service/report_service.go
+++ internal/service/report_service.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"mime/multipart"
 	"pantau/internal/dto/upload"
 	"pantau/internal/repository"
@@ -24,6 +26,35 @@ func NewReportService(reportRepo repository.ReportRepository) ReportService {
 func (sv *reportServiceImpl) uploadAll(
 	ctx context.Context,
 	files []*multipart.FileHeader,
-) ([]*upload.UploadResponse, error) {
-	panic("unimplemented")
+) ([]upload.UploadResponse, error) {
+	uploads := make([]upload.UploadResponse, 0, len(files))
+
+	for _, file := range files {
+		upload, err := sv.UploadService.Upload(ctx, file)
+		if err != nil {
+			rollbackErr := sv.deleteUploads(ctx, uploads)
+
+			if rollbackErr != nil {
+				return nil, fmt.Errorf("upload failed: %w; rollback failed: %v", err, rollbackErr)
+			}
+
+			return nil, err
+		}
+
+		uploads = append(uploads, *upload)
+	}
+
+	return uploads, nil
+}
+
+func (sv *reportServiceImpl) deleteUploads(ctx context.Context, uploads []upload.UploadResponse) error {
+	var rollbackErr error
+
+	for _, upload := range uploads {
+		if err := sv.UploadService.Delete(ctx, upload.ID); err != nil {
+			rollbackErr = errors.Join(rollbackErr, err)
+		}
+	}
+
+	return rollbackErr
 }
